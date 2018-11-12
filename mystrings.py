6930c1e864d50e0d95ca1587f798a0fff765de4e
root <root@ip-172-31-29-52.ap-northeast-2.compute.internal>
2018-11-12 09:32:35 +0000

    adding mystring.py

---
+++ mystrings.py
@@ -0,0 +1,13 @@
+s = "zekeLabs"
+print (s[-8:-3:1])
+print (s[:-3:1])
+print (s[::1])
+print (s[::-1])
+x = (s[-8:-3:1])
+
+z = s.replace("L","l")
+print(z)
+print(s.center(24,"#"))
+
+m = input ("Give me a string: ")
+print(m.center(25,"#"))
